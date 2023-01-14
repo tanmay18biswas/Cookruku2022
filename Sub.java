af887424e165c04631139c5c42bcf154e1582d7e
Tanmay Biswas <20tanmay01biswas@gmail.com>
2023-01-14 21:18:53 +0530

    modified sub.java

--- Sub.java
+++ Sub.java
@@ -2,6 +2,6 @@ public class Subtract{
     public static void main(String[] args){
         int a = 15;
         int b = 8;
-        System.out.println(a-b);
+        System.out.println("The result of Subtraction: "+(a-b));
     }
 }  
